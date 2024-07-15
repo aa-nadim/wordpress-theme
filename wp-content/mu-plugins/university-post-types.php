051ff4becfc423e9e9a29b771a555d36723a3fd0
Nadim <nadim@themeperch.net>
2024-07-15 16:18:12 +0600

    Quick import files and edit some-- previously forgotten

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -3,19 +3,16 @@ function university_post_types()
 {
     // Event Post Type
     register_post_type('event', array(
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor', 'excerpt'),
-        'rewrite' => array('slug' => 'events'),
-        'has_archive' => true,
         'public' => true,
+        'show_in_rest' => true,
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add New Event',
             'edit_item' => 'Edit Event',
-            'all_item' => 'All Events',
+            'all_items' => 'All Events',
             'singular_name' => 'Event'
         ),
-        'menu_icon' => 'dashicons-calendar',
+        'menu_icon' => 'dashicons-calendar'
     ));
 
     // Program Post Type
